f17212aa9d2611e794cf63496f2d74a167175ee9
Lauranne Delsirie <leeloodel@live.fr>
2020-06-16 15:37:47 +0200

    models and seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# require 'json'
+# require 'open-uri'
+
+# url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+# seed_serialized = open(url).read
+# seed = JSON.parse(seed_serialized)
+
+# puts "#{user['name']} - #{user['bio']}"
+
+Ingredient.create(name: "lemon")
+Ingredient.create(name: "ice")
+Ingredient.create(name: "mint leaves")
+
+Cocktail.create(name: "Mojito")
+Cocktail.create(name: "Jus de citron")
+
+Dose.create(description: "Classique et intemporel cocktail", cocktail: 1, ingredient: 1)
